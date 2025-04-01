8d7a4e59df5aa95da526253e34984650016580b4
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-01 23:14:36 +0400

    pure components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,55 +1,25 @@
 "use client";
-import { useState } from "react";
 
-export default function Form() {
-  const [form, setForm] = useState({
-    firstName: "Irakli",
-    lastName: "Ambroladze",
-    email: "ambroladzirakli@gmail.com",
-  });
+function Recipe({ drinkers }) {
   return (
-    <>
-      <label>
-        First name:{" "}
-        <input
-          className="border"
-          value={form.firstName}
-          onChange={(e) =>
-            setForm((f) => {
-              return { ...f, firstName: e.target.value };
-            })
-          }
-        />
-      </label>
-      <br />
-      <label>
-        Last name:
-        <input
-          className="border"
-          value={form.lastName}
-          onChange={(e) =>
-            setForm((f) => {
-              return { ...f, lastName: e.target.value };
-            })
-          }
-        />
-      </label>
-      <br />
-      <label>
-        Email:{" "}
-        <input
-          className="border"
-          value={form.email}
-          onChange={(e) =>
-            setForm((f) => {
-              return { ...f, email: e.target.value };
-            })
-          }
-        />
-      </label>
-      <p>
-        {form.firstName} {form.lastName} ({form.email})
-      </p>
-    </>
+    <ol>
+      <li>Boil {drinkers} cups of water.</li>
+      <li>
+        Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
+      </li>
+      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+    </ol>
+  );
+}
+
+export default function App() {
+  return (
+    <section>
+      <h1>Spiced Chai Recipe</h1>
+      <h2>For two</h2>
+      <Recipe drinkers={2} />
+      <h2>For gathering</h2>
+      <Recipe drinkers={4} />
+    </section>
   );
 }
