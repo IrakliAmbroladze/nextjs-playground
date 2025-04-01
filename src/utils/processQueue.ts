892c658f2cf410342e4d725eb195f9c18bafe33a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-01 13:59:17 +0400

    Challenge 2 of 2: Implement the state queue yourself. book version

--- src/utils/processQueue.ts
+++ src/utils/processQueue.ts
@@ -1,10 +1,10 @@
 export function getFinalState(baseState: number, queue: []) {
   let finalState = baseState;
   for (const entry of queue) {
-    if (typeof entry === "number") {
-      finalState = entry;
-    } else {
+    if (typeof entry === "function") {
       finalState = entry(finalState);
+    } else {
+      finalState = entry;
     }
   }
   return finalState;
