7919e977fabc93a8c2c5e97fb3d4b4cd2548a171
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-01 19:19:17 +0400

    flushSync. not recommended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,33 @@
 "use client";
+import { useState, useEffect } from "react";
+import { flushSync } from "react-dom";
 
-import { getFinalState } from "@/utils/processQueue";
+const PrintApp = () => {
+  const [isPrinting, setIsPrinting] = useState(false);
 
-interface TestCase {
-  baseState: number;
-  queue: number[] | string[];
-  expected: number;
-}
+  useEffect(() => {
+    function handleBeforePrint() {
+      flushSync(() => {
+        setIsPrinting(true);
+      });
+    }
+    function handleAfterPrint() {
+      setIsPrinting(false);
+    }
+    window.addEventListener("beforeprint", handleBeforePrint);
+    window.addEventListener("afterprint", handleAfterPrint);
+    return () => {
+      window.removeEventListener("beforeprint", handleBeforePrint);
+      window.removeEventListener("afterprint", handleAfterPrint);
+    };
+  }, []);
 
-function increment(n: number): number {
-  return n + 1;
-}
-increment.toString = () => "n => n+1";
-
-const TestCase = ({ baseState, queue, expected }: TestCase) => {
-  const actual = getFinalState(baseState, queue);
   return (
     <>
-      <p>
-        Base state: <b>{baseState}</b>
-      </p>
-      <p>
-        Queue: <b>[{queue.join(", ")}]</b>
-      </p>
-      <p>
-        Expected result: <b>{expected}</b>
-      </p>
-      <p
-        className={`${actual === expected ? "text-green-600" : "text-red-600"}`}
-      >
-        Your result: <b>{actual}</b> (
-        {actual === expected ? "correct" : "wrong"})
-      </p>
+      <h1>isPrinting: {isPrinting ? "yes" : "no"}</h1>
+      <button onClick={() => window.print()}>Print</button>
     </>
   );
 };
 
-const App = () => {
-  return (
-    <>
-      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[increment, increment, increment]}
-        expected={3}
-      />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment]} expected={6} />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
-    </>
-  );
-};
-
-export default App;
+export default PrintApp;
