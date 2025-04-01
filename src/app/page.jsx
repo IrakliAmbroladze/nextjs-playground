b0c761e103b804943ed268c5d86c77a222e4f898
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-01 20:46:34 +0400

    Move dynamic objects and functions inside your Effect

---
+++ src/app/page.jsx
@@ -0,0 +1,48 @@
+"use client";
+import { createConnection } from "@/utils/chat";
+import { useState, useEffect } from "react";
+
+const serverUrl = "https://localhost:1234";
+
+const ChatRoom = ({ roomId }) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const options = {
+      serverUrl,
+      roomId,
+    };
+    const connection = createConnection(options);
+    connection.connect();
+    return () => connection.disconnect();
+  }, [roomId]);
+  return (
+    <>
+      <h1 className="text-xl">Welcome to the {roomId} room!</h1>
+      <input
+        className="border"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+    </>
+  );
+};
+const App = () => {
+  const [roomId, setRoomId] = useState("general");
+  return (
+    <>
+      <label>
+        Choose the chat room:{" "}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <hr />
+      <ChatRoom roomId={roomId} />
+    </>
+  );
+};
+
+export default App;
