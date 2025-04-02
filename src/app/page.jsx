b0509ce9f0e3cc135afd576354a82cc4c211832e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-04-02 07:37:49 +0400

    Local mutation:

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,13 @@
 "use client";
 
-function Recipe({ drinkers }) {
-  return (
-    <ol>
-      <li>Boil {drinkers} cups of water.</li>
-      <li>
-        Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
-      </li>
-      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
-    </ol>
-  );
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
 }
 
 export default function App() {
-  return (
-    <section>
-      <h1>Spiced Chai Recipe</h1>
-      <h2>For two</h2>
-      <Recipe drinkers={2} />
-      <h2>For gathering</h2>
-      <Recipe drinkers={4} />
-    </section>
-  );
+  let cups = [];
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />);
+  }
+  return cups;
 }
